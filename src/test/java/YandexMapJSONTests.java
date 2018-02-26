164b15f4497a47996fcf1dc58e56d32ea0efbde8
mukhlygina <m.mukhlygina@gmail.com>
2018-02-26 18:35:02 +0300

    fix

--- src/test/java/YandexMapJSONTests.java
+++ src/test/java/YandexMapJSONTests.java
@@ -1,12 +1,28 @@
+import beans.YandexMapAnswer;
 import core.YandexMapsApi;
 import org.testng.annotations.Test;
 
+import static org.testng.Assert.assertEquals;
+
 public class YandexMapJSONTests {
-    private final String address = "Москва, улица Новый Арбат, дом 24";
+    private final String address = "Москва, улица Новый Арбат, 24";
     private final String format = "json";
+    private final String position = "37.587614 55.753083";
 
     @Test
-    public void mainTest() {
-        YandexMapsApi.with().format(format).geocode(address).callApi();
+    public void baseTest() {
+       YandexMapAnswer ans = YandexMapsApi.getYandexMapsAnswers(YandexMapsApi
+               .with().format(format).geocode(address).callApi());
+
+       ans.response.geoObjectCollection.featureMember.forEach(obj -> assertEquals(obj.geoObject.point.pos, position));
+    }
+
+    @Test
+    public void baseTest2() {
+        YandexMapAnswer ans = YandexMapsApi.getYandexMapsAnswers(YandexMapsApi
+                .with().format(format).geocode(position).callApi());
+
+        ans.response.geoObjectCollection.featureMember.forEach(
+                obj -> assertEquals(obj.geoObject.metaDataProperty.geocoderMetaData.address.formatted, address));
     }
 }
